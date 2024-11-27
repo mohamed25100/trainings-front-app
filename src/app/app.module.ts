0af6bf99bb3fc4b7123bc0de43d597a0376660e3
Mohamed BOUCHERBA <mboucherba31@gmail.com>
2024-11-27 15:54:35 +0100

    pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ModalOrderComponent } from './components/order/modal-order/modal-order.
 import { AdminComponent } from './components/admin/admin.component';
 import { LoginComponent } from './components/login/login.component';
 import { AddEditComponent } from './components/add-edit/add-edit.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
@@ -26,14 +27,15 @@ import { AddEditComponent } from './components/add-edit/add-edit.component';
     ModalOrderComponent,
     AdminComponent,
     LoginComponent,
-    AddEditComponent
+    AddEditComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/trainings/trainings.component.ts
+++ src/app/components/trainings/trainings.component.ts
@@ -12,12 +12,18 @@ import { CartService } from 'src/app/services/cart.service';
 export class TrainingsComponent implements OnInit {
   listTrainings : Training[] | undefined;
   error : string | undefined | null;
+  itemsPerPage = 3;
+  currentPage = 1;
   constructor(private cartService : CartService,private apiService : ApiService,private router : Router) { }
 
   ngOnInit(): void {
     this.getAllTrainings();
   }
 
+  /**
+   * méthode permettant d'ajouter la formation au panier
+   * @param training 
+   */
   onAddToCart(training:Training){
     if(training.quantity > 0 && training.quantity <=10) {
     this.cartService.addTraining(training);
@@ -27,11 +33,25 @@ export class TrainingsComponent implements OnInit {
       alert("la quantité dois être comprise entre 1 et 10");
     }
   }
+  /**
+   * méthode permettant de recuperer les formations
+   */
   getAllTrainings() {
     this.apiService.getTrainings().subscribe({
       next : (data) => this.listTrainings = data,
       error : (err) => this.error = err.message,
       complete : () => this.error = null
     })
+  }
+
+  get paginatedData() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+
+    return this.listTrainings?.slice(start,end);
+  }
+
+  changePage(page: number){
+    this.currentPage = page;
   }
 }
